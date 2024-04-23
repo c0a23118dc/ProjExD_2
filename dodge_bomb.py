137e56d34bdfcdf4ab9aa067fc54f3c3c54e718d
c0a23118dc <c0a23118dc@edu.teu.ac.jp>
2024-04-23 15:17:00 +0900

    練習

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,4 +1,5 @@
 import os
+import random
 import sys
 import pygame as pg
 
@@ -6,46 +7,72 @@ import pygame as pg
 WIDTH, HEIGHT = 1200, 600  # 定数なので大文字
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-#辞書を定義
-DELTA = dict{
-    pg.K_UP : (0, -5)
-    pg.K_DOWN : (0, 5)
-    pg.L_LEFT : (-5, 0)
-    pg.K_RIGHT : (5, 0)
+#移動量辞書
+DELTA = {
+    pg.K_UP : (0, -5),
+    pg.K_DOWN : (0, 5),
+    pg.K_LEFT : (-5, 0),
+    pg.K_RIGHT : (5, 0),
 }
+
+def check_bound(obj_rct:pg.Rect) -> tuple[bool, bool]:
+    """
+    こうかとんRect，または，爆弾Rectの画面内外判定用の関数
+    引数：こうかとんRect，または，爆弾Rect
+    戻り値：横方向判定結果，縦方向判定結果（True：画面内／False：画面外）
+    """
+    yoko, tate = True, True
+    if obj_rct.left < 0 or WIDTH < obj_rct.right: 
+        yoko = False
+    if obj_rct.top < 0 or HEIGHT < obj_rct.bottom:
+        tate = False
+    return yoko, tate
 
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
-    bg_img = pg.image.load("fig/pg_bg.jpg")    # 背景画像
+    # ここからこうかとんの設定
+    bg_img = pg.image.load("fig/pg_bg.jpg")    
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 2.0)
-    kk_rct = kk_img.get_rect()  # surfaceを抽出
-    kk_rct.center = 900, 400   # 中心座標
+    kk_rct = kk_img.get_rect()
+    kk_rct.center = 900, 400
+    # ここから爆弾の設定
+    bd_img = pg.Surface((20, 20))
+    bd_img.set_colorkey((0, 0, 0))
+    pg.draw.circle(bd_img, (255, 0, 0), (10, 10), 10)
+    bd_rct = bd_img.get_rect()
+    bd_rct.center = random.randint(0, WIDTH), random.randint(0, HEIGHT)
+    vx, vy = +5, +5  # 横方向速度，縦方向速度
     clock = pg.time.Clock()
     tmr = 0
     while True:
-        for event in pg.event.get():  # ゲームの×ボタンが押されたら終了
+        for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
         screen.blit(bg_img, [0, 0]) 
-
+        # こうかとんの移動と表示
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
-        if key_lst[pg.K_UP]:
-            sum_mv[1] -= 5   # 移動量
-        if key_lst[pg.K_DOWN]:
-            sum_mv[1] += 5
-        if key_lst[pg.K_LEFT]:
-            sum_mv[0] -= 5
-        if key_lst[pg.K_RIGHT]:
-            sum_mv[0] += 5
+        for k, v in DELTA.items():
+            if key_lst[k]:
+               sum_mv[0] += v[0]
+               sum_mv[1] += v[1]
         kk_rct.move_ip(sum_mv)
+        if check_bound(kk_rct) != (True, True):
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
         screen.blit(kk_img, kk_rct)
+        # 爆弾の移動と表示
+        bd_rct.move_ip(vx, vy)       
+        screen.blit(bd_img, bd_rct)
+        yoko, tate = check_bound(bd_rct)
+        if not yoko:  # 横方向にはみ出てたら
+            vx *= -1
+        if not tate:  # 縦方向にはみ出てたら
+            vy *= -1
         pg.display.update()
         tmr += 1
         clock.tick(50)
-
 
 if __name__ == "__main__":
     pg.init()
